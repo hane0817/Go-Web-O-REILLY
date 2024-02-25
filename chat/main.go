dff0f33e83fbfc602dbb85ad462d39b8a1a9e90f
hane <winwinmaomao@gmail.com>
2024-02-25 23:01:06 +0900

    ポート番号の指定を実装

--- chat/main.go
+++ chat/main.go
@@ -1,35 +1,42 @@
 package main
 
 import (
+	"flag"
+	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
 	"sync"
-	"text/template"
 )
 
+// templは1つのテンプレートを表します
 type templateHandler struct {
 	once     sync.Once
 	filename string
 	templ    *template.Template
 }
 
-// ServeHTTP handles the HTTP request.
+// ServeHTTPはHTTPリクエストを処理します
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
-		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("templates",
+				t.filename)))
 	})
 	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse() // フラグを解釈します
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
-	//チャットルームを開始します
+	// チャットルームを開始します
 	go r.run()
-	// start web server
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	// Webサーバーを起動します
+	log.Println("Webサーバーを開始します。ポート", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
