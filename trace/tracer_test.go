a7d7bfe1e95a41078f86cf50ab9b287674d6f267
hane <winwinmaomao@gmail.com>
2024-02-26 23:40:07 +0900

    テストの練習

---
+++ trace/tracer_test.go
@@ -0,0 +1,18 @@
+package trace
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("Newからの戻り値がnilです")
+	}
+	tracer.Trace("こんにちは，traceパッケージ")
+	if buf.String() != "こんにちは，traceパッケージ\n" {
+		t.Error("'%s'という誤った文字列が出力されました", buf.String())
+	}
+}
